5a577ef4c9ecc1b8ba03124fa79e1b45cdd6e232
Vonlery Villanueva <vonleryv@gmail.com>
2025-12-02 11:24:23 +0800

    loginpage
    w/ go back link

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <section>
+      <Link href="/">Go back</Link>
+      <h1 className="">Login Page</h1>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="">hello glinarddd</div>;
+  return <div className="">Home</div>;
 }
