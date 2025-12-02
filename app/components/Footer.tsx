11038e060774f9415b42e547b543da52eee6a82b
Vonlery Villanueva <vonleryv@gmail.com>
2025-12-02 14:57:31 +0800

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Logo from "./Logo";
-
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center h-max">
       <h2>
         Challenge or Solution from
         <span className="text-[var(--default)]"> CodeCraft</span> Created by
-        <span className="text-[var(--default)]"> Vonlery Villanueva</span>
+        <span className="text-[var(--default)]">
+          {" "}
+          Vonlery Villanueva&apos;s
+        </span>
       </h2>
     </footer>
   );
